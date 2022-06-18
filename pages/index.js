963200dc39041fac9b98f0b919fde5dda48a829f
Sabber-Soltani <96610564+sabber-slt@users.noreply.github.com>
2022-06-18 17:00:07 +0430

    asdas

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Box } from '@chakra-ui/react';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
@@ -13,7 +14,7 @@ export default function Home() {
     if (user === null) {
       router.push('/register');
     }
-  }, [user, router]);
+  }, []);
   return (
     <Box>
       <Header language={user ? user.language : 'en'} />
